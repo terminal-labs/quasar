ae0b7e700a3c469cb70ee4b27cb5423ed897432e
Michael Verhulst <michael@terminallabs.com>
2020-12-31 17:29:50 -0600

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,32 +1,8 @@
 import os
-import sys
-import click
-import pytest
 
-@click.group()
-def cli():
-    return None
+with open(os.path.dirname(__file__) + "/framework/loader.py") as f:
+    code = compile(f.read(), "loader.py", "exec")
+    exec(code)
 
-
-@click.group(name="system")
-def system_group():
-    return None
-
-
-@system_group.command(name="version")
-def version_command():
-    print("not implemented")
-
-
-@system_group.command(name="selftest")
-def selftest_command():
-    print("not implemented")
-
-
-@system_group.command(name="selfcoverage")
-def selfcoverage_command():
-    print("not implemented")
-
-
-cli.add_command(system_group)
-main = cli
+_pgk_name = _get_pgk_name()
+main = _import_fun(f"{_pgk_name}.cli", "main")
